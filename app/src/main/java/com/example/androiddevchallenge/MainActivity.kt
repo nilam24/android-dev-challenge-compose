a5acb9b1c9c33a43e35fe0deecda1dadc0aad6a2
nilam24 <negpa.nilam@gmail.com>
2021-03-10 01:06:02 +0530

    first commit

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,25 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +47,98 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Preview
+@Composable
+fun ImageForCountdown() {
+    MaterialTheme {
+        Column(modifier = Modifier.absolutePadding(16.dp, 16.dp, 6.dp, 6.dp)) {
+            countDownTimerTask()
+            ImageUp()
+            ImageDwn()
+
+        }
+
+    }
+}
+
 // Start building your app here!
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        //  Text(text = "Ready... Set... GO!")
+        ImageForCountdown()
     }
 }
+
+@Composable
+fun countDownTimerTask() {
+    var setProgress: String by remember { mutableStateOf(value = "Count down") }
+    var num: Long by remember { mutableStateOf(value = 10) }
+
+    val countDownTimer = object : CountDownTimer(10000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            num = millisUntilFinished / 1000
+            setProgress = "$num"
+            Log.e("timercounttick", "" + millisUntilFinished)
+
+
+        }
+
+        override fun onFinish() {
+            setProgress = "finished"
+
+        }
+    }
+    Button(onClick = {
+        countDownTimer.start()
+    }) {
+        Text(text = "Start Count Down")
+
+    }
+    ProgressImagedwn(setProgress = setProgress)
+
+}
+
+@Composable
+fun ImageUp() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.img3),
+            contentDescription = null,
+            modifier = Modifier
+                .height(79.dp)
+                .width(69.dp),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds
+        )
+
+    }
+}
+
+@Composable
+fun ProgressImagedwn(setProgress: String) {
+
+    Text(text = "$setProgress", modifier = Modifier.padding(6.dp), style = typography.h5)
+
+}
+
+@Composable
+fun ImageDwn() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.img11),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .height(79.dp)
+                .width(69.dp)
+        )
+
+
+    }
+}
+
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
