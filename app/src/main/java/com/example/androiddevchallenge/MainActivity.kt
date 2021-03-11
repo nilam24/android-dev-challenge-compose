9a6381278bdd2d0e7cf672229a4ec1636db1e37e
nilam24 <negpa.nilam@gmail.com>
2021-03-11 23:04:27 +0530

    second

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,14 +20,17 @@ import android.os.CountDownTimer
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
@@ -50,8 +53,8 @@ fun ImageForCountdown() {
     MaterialTheme {
         Column(modifier = Modifier.absolutePadding(16.dp, 16.dp, 6.dp, 6.dp)) {
             CountDownTimerTask()
-//            ImageUp()
-//            ImageDwn()
+            ImageUp()
+            ImageDwn()
 
         }
 
@@ -96,45 +99,45 @@ fun CountDownTimerTask() {
 
 }
 
-//@Composable
-//fun ImageUp() {
-//    Box {
-//        Image(
-//            painter = painterResource(id = R.drawable.img3),
-//            contentDescription = null,
-//            modifier = Modifier
-//                .height(79.dp)
-//                .width(69.dp),
-//            alignment = Alignment.Center,
-//            contentScale = ContentScale.FillBounds
-//        )
-//
-//    }
-//}
+@Composable
+fun ImageUp() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.img3),
+            contentDescription = null,
+            modifier = Modifier
+                .height(79.dp)
+                .width(69.dp),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds
+        )
+
+    }
+}
 
 @Composable
 fun ProgressImage(setProgress: String) {
 
-    Text(text = setProgress, modifier = Modifier.absolutePadding(6.dp,6.dp,6.dp,6.dp), style = typography.h5)
+    Text(text = setProgress, modifier = Modifier.padding(6.dp), style = typography.h5)
 
 }
 
-//@Composable
-//fun ImageDwn() {
-//    Box {
-//        Image(
-//            painter = painterResource(id = R.drawable.img11),
-//            contentDescription = null,
-//            alignment = Alignment.Center,
-//            contentScale = ContentScale.FillBounds,
-//            modifier = Modifier
-//                .height(79.dp)
-//                .width(69.dp)
-//        )
-//
-//
-//    }
-//}
+@Composable
+fun ImageDwn() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.img11),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .height(79.dp)
+                .width(69.dp)
+        )
+
+
+    }
+}
 
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
